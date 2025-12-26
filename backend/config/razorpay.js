b55d825dbc24c10f77d11069c62aa7cba740e8f3
Aadarshh09 <166731853+Aadarshh09@users.noreply.github.com>
2025-12-26 17:42:56 +0530

    Update razorpay.js

--- backend/config/razorpay.js
+++ backend/config/razorpay.js
@@ -1,7 +1,12 @@
 const Razorpay = require("razorpay");
 
+if (!process.env.RAZORPAY_KEY_ID) {
+  throw new Error("Razorpay key missing from environment");
+}
 
-exports.instance = new Razorpay({
-   key_id: process.env.RAZORPAY_KEY,
-   key_secret: process.env.RAZORPAY_SECRET,
- });
+const razorpayInstance = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+module.exports = razorpayInstance;
